0e48d2298df2a681a1b61dd8415432a658f18b13
Ahmad Rulim <personal@rulim34.dev>
2023-04-16 13:43:39 +0700

    project: Remove unused i18n config
    Signed-off-by: Ahmad Rulim <personal@rulim34.dev>

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['id-ID'],
-    defaultLocale: 'id-ID',
-  },
   images: {
     remotePatterns: [
       {
