4f3b9c809915d19e27ffff132848fe496b2021b3
Muhammad Naufal <me@falkia34.dev>
2025-05-20 22:25:30 +0700

    fix(config): Remove MUI modularize imports
    Already modularized by Next.js
    
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- next.config.js
+++ next.config.js
@@ -10,12 +10,4 @@ module.exports = {
       },
     ],
   },
-  modularizeImports: {
-    '@mui/material': {
-      transform: '@mui/material/{{member}}',
-    },
-    '@mui/icons-material': {
-      transform: '@mui/icons-material/{{member}}',
-    },
-  },
 };
