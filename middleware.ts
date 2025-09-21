bcd8e08efadcddd8d0d78447d1b0aeba8064fd75
Muhammad Naufal <me@falkia34.dev>
2025-09-21 15:28:39 +0700

    fix(middleware): Change middleware runtime to nodejs
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- middleware.ts
+++ middleware.ts
@@ -14,5 +14,6 @@ export default authDataSource.auth((req) => {
 });
 
 export const config = {
+  runtime: 'nodejs',
   matcher: ['/((?!auth|login|logout|_next/static|_next/image|favicon.ico).*)'],
 };
