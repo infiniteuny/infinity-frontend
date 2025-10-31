0d4e8d1a84b2aea663a096c1d32af665598e2c30
Muhammad Naufal <me@falkia34.dev>
2025-11-01 01:23:27 +0700

    fix(component): Update skip button to use filled variant
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- src/presentation/components/shared/skip-button.tsx
+++ src/presentation/components/shared/skip-button.tsx
@@ -4,8 +4,8 @@ export function SkipToContentButton() {
   return (
     <Button
       href="#content"
-      variant="outlined"
-      className="absolute top-2 -left-96 z-[-99] focus:left-2 focus:z-[1500] active:left-2 active:z-[1500]"
+      variant="filled"
+      className="absolute top-2 -left-96 z-[-99] focus:left-2 focus:z-1500 active:left-2 active:z-1500"
     >
       Skip to content
     </Button>
