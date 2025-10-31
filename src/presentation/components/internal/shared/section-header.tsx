295516964c2f0e0d80af70b632a7a6dc694f4209
Muhammad Naufal <me@falkia34.dev>
2025-11-01 01:24:14 +0700

    feat(component): Update section header to accommodate menu
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- src/presentation/components/internal/shared/section-header.tsx
+++ src/presentation/components/internal/shared/section-header.tsx
@@ -1,15 +1,18 @@
+import { ReactNode } from 'react';
 import { Toolbar, Typography } from '@mui/material';
 
 type Props = {
   title: string;
+  children?: ReactNode;
 };
 
-export function SectionHeader({ title }: Props) {
+export function SectionHeader({ title, children }: Props) {
   return (
     <Toolbar component="header" className="p-6">
-      <Typography component="h1" variant="h5" className="font-medium">
+      <Typography component="h1" variant="h5" className={`${children ? 'mr-4' : ''} font-medium`}>
         {title}
       </Typography>
+      {children}
     </Toolbar>
   );
 }
