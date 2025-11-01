387a750f22fa823456b48654b785a0d63668893d
Muhammad Naufal <me@falkia34.dev>
2025-11-01 09:00:27 +0700

    feat(route): Add single user page
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- app/(internal)/(dashboard)/users/[userId]/page.tsx
+++ app/(internal)/(dashboard)/users/[userId]/page.tsx
@@ -1,9 +1,46 @@
+import { GetUser } from '@app/application';
+import { match } from 'effect/Either';
+import { SectionHeader } from '@app/presentation/components/internal/shared';
+import { serverContainer } from '@app/server-injection';
+import { SYMBOLS } from '@config';
+import { User } from '@app/domain/entities';
+import { UserDto, UserMapper } from '@app/infrastructure/dtos';
+import { UserForm, UserView } from '@app/presentation/components/internal/single-user';
+
 type Props = {
   params: Promise<{
     userId: string;
   }>;
 };
 
-export default function SingleUserPage({ params }: Props) {
-  return <section></section>;
+export default async function SingleUserPage({ params }: Props) {
+  const getUser = serverContainer.get<GetUser>(SYMBOLS.GetUser);
+  const userId = (await params).userId;
+
+  let user: User | undefined;
+  if (userId !== 'new') {
+    const result = await getUser.execute(userId, ['major']);
+    user = match(result, {
+      onLeft: (error) => {
+        throw error;
+      },
+      onRight: (data) => data,
+    });
+  }
+
+  if (user) {
+    return (
+      <>
+        <SectionHeader title={user.name} />
+        <UserView initialUser={UserMapper.fromDomaintoDto(user) as UserDto} />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <SectionHeader title="Create User" />
+        <UserForm />
+      </>
+    );
+  }
 }
