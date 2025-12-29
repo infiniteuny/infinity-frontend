521ba34c54e1e33b8fb05767120af1eef3819db0
Muhammad Naufal <me@falkia34.dev>
2025-12-30 00:41:00 +0700

    fix: Update Infinity API timeout env var
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- src/infrastructure/datasources/infinity-api.data-source.ts
+++ src/infrastructure/datasources/infinity-api.data-source.ts
@@ -6,5 +6,7 @@ export interface InfinityApiDataSource extends AxiosInstance {}
 export const infinityApiDataSourceImpl: InfinityApiDataSource = axios.create({
   baseURL: process.env.NEXT_PUBLIC_INFINITY_API_URL,
   timeout:
-    process.env.INFINITY_API_TIMEOUT != null ? Number(process.env.INFINITY_API_TIMEOUT) : 10000,
+    process.env.NEXT_PUBLIC_INFINITY_API_TIMEOUT != null
+      ? Number(process.env.NEXT_PUBLIC_INFINITY_API_TIMEOUT)
+      : 10000,
 });
