b20d5c2d643ad583273768d1c95438d59df98b12
Muhammad Naufal <me@falkia34.dev>
2026-04-11 14:18:16 +0700

    fix(eslint): Remove duplicated eslint plugin
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ const { defineConfig, globalIgnores } = require('eslint/config');
 const nextCoreWebVitals = require('eslint-config-next/core-web-vitals');
 const nextTs = require('eslint-config-next/typescript');
 const prettierRecommended = require('eslint-plugin-prettier/recommended');
-const typescriptEslintPlugin = require('@typescript-eslint/eslint-plugin');
 
 module.exports = defineConfig([
   ...nextCoreWebVitals,
@@ -11,9 +10,6 @@ module.exports = defineConfig([
   prettierRecommended,
   {
     files: ['**/*.ts', '**/*.tsx'],
-    plugins: {
-      '@typescript-eslint': typescriptEslintPlugin,
-    },
     rules: {
       'no-prototype-builtins': 'off',
       '@typescript-eslint/explicit-member-accessibility': 'error',
