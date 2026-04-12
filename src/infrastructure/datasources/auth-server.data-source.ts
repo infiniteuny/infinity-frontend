60690ee6f2cf51dd32bee226f500a25ef59a5fed
Muhammad Naufal <me@falkia34.dev>
2026-04-12 17:32:06 +0700

    fix(datasource): Use uuid
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- src/infrastructure/datasources/auth-server.data-source.ts
+++ src/infrastructure/datasources/auth-server.data-source.ts
@@ -108,6 +108,9 @@ export const authServerDataSourceImpl = (getUsers: GetUsersUnauthenticated) => {
     },
     advanced: {
       cookiePrefix: 'auth',
+      database: {
+        generateId: 'uuid',
+      },
     },
   });
 };
