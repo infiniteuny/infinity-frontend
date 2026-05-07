7d2c36f384906de943904810d3471d83692dd2d0
Muhammad Naufal <me@falkia34.dev>
2026-05-08 02:19:02 +0700

    fix(datasource): Check for window type for client side datasource to make sure not triggered on server side
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- src/infrastructure/datasources/local-storage.data-source.ts
+++ src/infrastructure/datasources/local-storage.data-source.ts
@@ -9,14 +9,20 @@ export interface LocalStorageDataSource {
 @injectable()
 export class LocalStorageDataSourceImpl implements LocalStorageDataSource {
   public get<T>(key: string): T {
-    return JSON.parse(window.localStorage.getItem(key) || '{}');
+    const result = typeof window !== 'undefined' ? window.localStorage.getItem(key) : null;
+
+    return JSON.parse(result || '{}');
   }
 
   public set<T>(key: string, value: T): void {
-    window.localStorage.setItem(key, JSON.stringify(value));
+    if (typeof window !== 'undefined') {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    }
   }
 
   public remove(key: string): void {
-    window.localStorage.removeItem(key);
+    if (typeof window !== 'undefined') {
+      window.localStorage.removeItem(key);
+    }
   }
 }
--- src/infrastructure/datasources/session-storage.data-source.ts
+++ src/infrastructure/datasources/session-storage.data-source.ts
@@ -9,14 +9,20 @@ export interface SessionStorageDataSource {
 @injectable()
 export class SessionStorageDataSourceImpl implements SessionStorageDataSource {
   public get<T>(key: string): T {
-    return JSON.parse(window.sessionStorage.getItem(key) || '{}');
+    const result = typeof window !== 'undefined' ? window.sessionStorage.getItem(key) : null;
+
+    return JSON.parse(result || '{}');
   }
 
   public set<T>(key: string, value: T): void {
-    window.sessionStorage.setItem(key, JSON.stringify(value));
+    if (typeof window !== 'undefined') {
+      window.sessionStorage.setItem(key, JSON.stringify(value));
+    }
   }
 
   public remove(key: string): void {
-    window.sessionStorage.removeItem(key);
+    if (typeof window !== 'undefined') {
+      window.sessionStorage.removeItem(key);
+    }
   }
 }
