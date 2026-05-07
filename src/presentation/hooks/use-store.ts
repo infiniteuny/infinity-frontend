369c764470e4ee3681f1063d02a25325cee298b2
Muhammad Naufal <me@falkia34.dev>
2026-05-08 00:50:56 +0700

    feat(store): Remove unused public store
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- src/presentation/hooks/use-store.ts
+++ src/presentation/hooks/use-store.ts
@@ -1,8 +1,7 @@
-import { createInternalStore, createPublicStore } from '@app/presentation/stores';
+import { createInternalStore } from '@app/presentation/stores';
 import { useShallow } from 'zustand/shallow';
 import { useStore } from 'zustand';
 
 export const internalStore = createInternalStore();
-export const publicStore = createPublicStore();
 
 export { useShallow, useStore };
--- src/presentation/stores/index.ts
+++ src/presentation/stores/index.ts
@@ -1,2 +1,1 @@
 export * from './create-internal-store';
-export * from './create-public-store';
