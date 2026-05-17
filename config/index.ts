d1174f1cbabae30d4bda213c9b496119252ade60
Muhammad Naufal <me@falkia34.dev>
2026-05-17 23:22:14 +0700

    fix(config): Add competitions in settings route path matcher
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- config/index.ts
+++ config/index.ts
@@ -162,7 +162,7 @@ export const APP: {
           icon: 'settings',
           path: '/settings',
           matcher:
-            '^\\/(settings|degrees|faculties|majors|personas|team-types|core-team-divisions|community-groups|competition-organizer-types|competition-outputs|competition-ranks|competition-scales|competition-time-ranges)',
+            '^\\/(settings|degrees|faculties|majors|personas|team-types|core-team-divisions|community-groups|competitions|competition-organizer-types|competition-scales|competition-time-ranges|competition-outputs|competition-ranks)',
         },
       ],
     },
