40600600cd439b9445ebb0b1ba9f16832755203c
Muhammad Naufal <me@falkia34.dev>
2026-06-07 14:08:40 +0700

    feat(component): Add breadcrumbs component to main
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- src/presentation/components/internal/shared/main.tsx
+++ src/presentation/components/internal/shared/main.tsx
@@ -1,14 +1,20 @@
 'use client';
 
-import { Box, Container } from '@mui/material';
+import NextLink from 'next/link';
+import { Box, Breadcrumbs, Container, Link, Typography } from '@mui/material';
 import { InternalStoreContext } from './store-provider';
 import { ReactNode, useContext, useSyncExternalStore } from 'react';
+import { NavigateNextRounded } from '@mui/icons-material';
 
 type Props = {
+  breadcrumbs?: {
+    label: string;
+    url: string;
+  }[];
   children: ReactNode;
 };
 
-export function InternalMain({ children }: Props) {
+export function InternalMain({ breadcrumbs, children }: Props) {
   const store = useContext(InternalStoreContext);
   const sidebarExtended = useSyncExternalStore(
     store!.subscribe,
@@ -18,7 +24,6 @@ export function InternalMain({ children }: Props) {
 
   return (
     <Container
-      id="content"
       component="main"
       maxWidth={false}
       sx={{ bgcolor: 'surfaceContainer.main' }}
@@ -26,7 +31,34 @@ export function InternalMain({ children }: Props) {
         sidebarExtended ? 'lg:pl-65' : 'lg:pl-20'
       }`}
     >
+      {breadcrumbs && breadcrumbs.length > 0 ? (
+        <Breadcrumbs
+          separator={<NavigateNextRounded fontSize="small" />}
+          maxItems={3}
+          aria-label="breadcrumb"
+          className="my-2 px-4"
+        >
+          {breadcrumbs.map((breadcrumb, index) =>
+            index === breadcrumbs.length - 1 ? (
+              <Typography sx={{ color: 'text.primary' }} key={index}>
+                {breadcrumb.label}
+              </Typography>
+            ) : (
+              <Link
+                component={NextLink}
+                key={index}
+                href={breadcrumb.url}
+                underline="hover"
+                color="text.disabled"
+              >
+                {breadcrumb.label}
+              </Link>
+            ),
+          )}
+        </Breadcrumbs>
+      ) : null}
       <Box
+        id="content"
         sx={{ bgcolor: 'surfaceContainerLow.main' }}
         className="flow-root h-full min-h-[calc(100vh-8.625rem)] w-full rounded-2xl"
       >
