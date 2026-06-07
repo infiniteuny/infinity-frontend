bc48a80cec4056800d729e13d0be1e2cd0740132
Muhammad Naufal <me@falkia34.dev>
2026-06-07 14:11:02 +0700

    feat(component): Add back button to section header
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- src/presentation/components/internal/shared/section-header.tsx
+++ src/presentation/components/internal/shared/section-header.tsx
@@ -1,17 +1,36 @@
+import Link from 'next/link';
+import { ArrowBackRounded } from '@mui/icons-material';
+import { Container, IconButton, Toolbar, Typography } from '@mui/material';
 import { ReactNode } from 'react';
-import { Toolbar, Typography } from '@mui/material';
 
 type Props = {
   title: string;
+  backUrl?: string;
   children?: ReactNode;
 };
 
-export function SectionHeader({ title, children }: Props) {
+export function SectionHeader({ title, backUrl, children }: Props) {
   return (
     <Toolbar component="header" className="items-start p-6">
-      <Typography component="h1" variant="h5" className={`${children ? 'mr-4' : ''} font-medium`}>
-        {title}
-      </Typography>
+      <Container
+        maxWidth={false}
+        disableGutters
+        className={`m-0 inline-flex w-auto items-center ${children ? 'mr-4' : ''}`}
+      >
+        {backUrl && (
+          <IconButton
+            component={Link}
+            size="small"
+            href={backUrl}
+            className="-my-2 mr-3 -ml-2 box-content inline-flex"
+          >
+            <ArrowBackRounded />
+          </IconButton>
+        )}
+        <Typography component="h1" variant="h5" className="inline font-medium">
+          {title}
+        </Typography>
+      </Container>
       {children}
     </Toolbar>
   );
